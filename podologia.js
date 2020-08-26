e3540e5dcbecf2879a07408df5b8c75a305a9887
mhjmhj2002 <mhjmhj2002@gmail.com>
2020-08-26 19:21:43 -0300

    Update podologia.js

--- podologia.js
+++ podologia.js
@@ -1,5 +1,5 @@
 var app = require('./config/custom-express')();
 
-app.listen(21137, function(){
-  console.log('Servidor rodando na porta 21137.');
+app.listen(21228, function(){
+  console.log('Servidor rodando na porta 21228.');
 });
