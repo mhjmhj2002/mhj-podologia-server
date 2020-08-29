6f162632f612a1f2a717834ed70b6b3d1a67aa2b
Mhj <mhjmhj2002@gmail.com>
2020-08-29 18:55:23 -0300

    add

--- podologia.js
+++ podologia.js
@@ -1,5 +1,5 @@
 var app = require('./config/custom-express')();
 
-app.listen(21526, function(){
-  console.log('Servidor rodando na porta 21526.');
+app.listen(21228, function(){
+  console.log('Servidor rodando na porta 21228.');
 });
